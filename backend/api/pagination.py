886fa687bbe19ccf533b7c6bd3045fa76d0ef18b
Artem <aleshES2@yandex.ru>
2026-09-17 21:42:13 +1000

    refactor

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -1,8 +1,8 @@
 from rest_framework.pagination import PageNumberPagination
 
-from foodgram.constants import PAGE_SIZE
+from foodgram.constants import LIMIT_QUERY_PARAM, PAGE_SIZE
 
 
 class PageLimitPagination(PageNumberPagination):
-    page_size_query_param = 'limit'
+    page_size_query_param = LIMIT_QUERY_PARAM
     page_size = PAGE_SIZE
